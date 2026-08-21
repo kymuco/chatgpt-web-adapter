88f180dff4e26604ae1e60c09ec63818e1d63560
Ikymuco <ikymuco@gmail.com>
2026-08-21 14:05:26 +0600

    fix: load PR8.13 Temporary session identity overlay

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_observability.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_observability.js
@@ -31,6 +31,7 @@ importScripts("service_worker_normalized_activity_stream_pr8_12.js");
 importScripts("service_worker_normalized_activity_patch_protocol_pr8_12.js");
 importScripts("service_worker_answer_channel_pr8_12.js");
 importScripts("service_worker_temporary_chat_production_pr8_13.js");
+importScripts("service_worker_temporary_session_identity_pr8_13.js");
 
 const _pr824aOriginalExecuteNativeTurn = executeNativeTurn;
 
