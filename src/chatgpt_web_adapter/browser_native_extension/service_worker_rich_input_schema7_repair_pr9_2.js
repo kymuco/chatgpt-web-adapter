e75a45d147f19d518e46cf684a7e144f3f4340ec
Ikymuco <ikymuco@gmail.com>
2026-08-29 02:08:46 +0600

    load schema 26 staging diagnostic

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
@@ -23,3 +23,4 @@ importScripts("service_worker_rich_input_schema24_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema23_diagnostic_pr9_2.js");
 importScripts("service_worker_rich_input_schema25_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema26_repair_pr9_2.js");
+importScripts("service_worker_rich_input_schema26_staging_diagnostic_pr9_2.js");
