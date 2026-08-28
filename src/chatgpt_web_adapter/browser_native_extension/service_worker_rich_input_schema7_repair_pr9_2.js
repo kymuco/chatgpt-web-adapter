01bd798663660faf765560867d762226a7d82e5e
Ikymuco <ikymuco@gmail.com>
2026-08-29 02:05:08 +0600

    load schema 26 ambiguity-safe removal repair

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
@@ -22,3 +22,4 @@ importScripts("service_worker_rich_input_schema23_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema24_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema23_diagnostic_pr9_2.js");
 importScripts("service_worker_rich_input_schema25_repair_pr9_2.js");
+importScripts("service_worker_rich_input_schema26_repair_pr9_2.js");
