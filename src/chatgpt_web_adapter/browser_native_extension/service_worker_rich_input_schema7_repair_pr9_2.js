88fa9c2cd2fcc068caa1dbac55da4b76494735d8
Ikymuco <ikymuco@gmail.com>
2026-08-28 22:47:27 +0600

    Load PR9.2 schema21 closure

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
@@ -16,3 +16,4 @@ importScripts("service_worker_rich_input_schema17_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema18_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema19_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema20_repair_pr9_2.js");
+importScripts("service_worker_rich_input_schema21_repair_pr9_2.js");
