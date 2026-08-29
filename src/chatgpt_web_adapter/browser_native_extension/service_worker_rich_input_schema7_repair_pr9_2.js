ffaa3bdd83b35360f756f3d2ea99ef9b697bd10f
Ikymuco <ikymuco@gmail.com>
2026-08-29 11:50:40 +0600

    PR9.2 load schema28 causal identity repair

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_rich_input_schema7_repair_pr9_2.js
@@ -26,3 +26,4 @@ importScripts("service_worker_rich_input_schema26_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema26_staging_diagnostic_pr9_2.js");
 importScripts("service_worker_rich_input_schema27_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema27_staging_diagnostic_pr9_2.js");
+importScripts("service_worker_rich_input_schema28_repair_pr9_2.js");
