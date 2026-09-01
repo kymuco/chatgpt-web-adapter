82770674f2c1113271697aab6f24c7e86bf49295
Ikymuco <ikymuco@gmail.com>
2026-09-01 23:49:48 +0600

    PR10.1 load artifact subtree v7 characterization

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_observability.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_observability.js
@@ -38,6 +38,7 @@ importScripts("service_worker_generated_artifact_topology_v3_pr10_1.js");
 importScripts("service_worker_generated_artifact_noncode_v4_pr10_1.js");
 importScripts("service_worker_generated_artifact_action_v5_pr10_1.js");
 importScripts("service_worker_generated_artifact_fiber_state_v6_pr10_1.js");
+importScripts("service_worker_generated_artifact_subtree_v7_pr10_1.js");
 importScripts("service_worker_normalized_activity_patch_protocol_pr8_12.js");
 importScripts("service_worker_answer_channel_pr8_12.js");
 importScripts("service_worker_temporary_chat_production_pr8_13.js");
