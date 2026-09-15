92d1d80a9261809717cf6d2729ace1c69c6d78e0
Ikymuco <ikymuco@gmail.com>
2026-09-15 17:58:35 +0600

    Fold canonical session auth into v2 reader

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime_read.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime_read.js
@@ -5,4 +5,3 @@
 
 importScripts("service_worker_product_source_citations_pr9_3.js");
 importScripts("service_worker_canonical_read_v2.js");
-importScripts("service_worker_canonical_read_session_auth.js");
