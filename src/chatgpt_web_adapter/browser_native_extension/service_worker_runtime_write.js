c7dcb0a757875f7712071740bf60c0b7c0560191
Ikymuco <ikymuco@gmail.com>
2026-09-16 23:10:32 +0600

    Use neutral browser indentation module name

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime_write.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime_write.js
@@ -11,7 +11,7 @@ importScripts("service_worker_rich_input_deadline_repair_pr9_2.js");
 importScripts("service_worker_rich_input_closure_repair_pr9_2.js");
 importScripts("service_worker_rich_input_schema7_repair_pr9_2.js");
 importScripts("service_worker_request_text_shape_compat.js");
-importScripts("service_worker_browser_indent_compat_pr14_3.js");
+importScripts("service_worker_browser_indent_compat.js");
 importScripts("service_worker_ui_compat_pr11_7.js");
 importScripts("service_worker_text_submit_commit_hardening_pr11_3.js");
 importScripts("service_worker_ordinary_text_identity_authority.js");
