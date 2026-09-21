ad3f49c9048708a696e60a752e6107f62fe461ab
Ikymuco <ikymuco@gmail.com>
2026-09-21 16:52:03 +0600

    PR15.14 update runtime root assembly contract

--- tests/test_browser_runtime_consolidation_pr12_0.py
+++ tests/test_browser_runtime_consolidation_pr12_0.py
@@ -47,6 +47,7 @@ def test_runtime_entrypoint_is_assembly_only_with_explicit_domain_order() -> Non
         'importScripts("service_worker_runtime_write.js");',
         'importScripts("service_worker_runtime_read.js");',
         'importScripts("service_worker_runtime_observation.js");',
+        'importScripts("service_worker_official_page_turn_lifecycle.js");',
         'importScripts("service_worker_native_turn_lifecycle.js");',
     ]
 
