128be5864e88493ed26f457c837b6d087a452261
Ikymuco <ikymuco@gmail.com>
2026-09-26 13:12:03 +0600

    PR15.55: assemble Gemini provider before native router

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime.js
@@ -14,6 +14,7 @@ importScripts("service_worker_runtime_tab_resolution.js");
 importScripts("service_worker_runtime_read.js");
 importScripts("service_worker_runtime_observation.js");
 importScripts("service_worker_deepseek_provider.js");
+importScripts("service_worker_gemini_provider.js");
 importScripts("service_worker_native_message_router.js");
 importScripts("service_worker_official_page_turn_lifecycle.js");
 importScripts("service_worker_native_turn_lifecycle.js");
