115db3204e1d8d0ee59c638f37cce4fcfdee0684
Ikymuco <ikymuco@gmail.com>
2026-09-26 16:49:04 +0600

    PR17.0: assemble role-named Google Translate worker

--- src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime.js
+++ src/chatgpt_web_adapter/browser_native_extension/service_worker_runtime.js
@@ -15,7 +15,7 @@ importScripts("service_worker_runtime_read.js");
 importScripts("service_worker_runtime_observation.js");
 importScripts("service_worker_deepseek_provider.js");
 importScripts("service_worker_gemini_provider.js");
-importScripts("service_worker_google_translate_pr17_0.js");
+importScripts("service_worker_google_translate.js");
 importScripts("service_worker_native_message_router.js");
 importScripts("service_worker_official_page_turn_lifecycle.js");
 importScripts("service_worker_native_turn_lifecycle.js");
